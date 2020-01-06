f4f415c4f2fa453d52780715f7952278a1d12e9c
Jordan Mahsman <31456960+Mahsman27@users.noreply.github.com>
2020-01-06 10:56:35 -0600

    Rock, Paper, Scissors

---
+++ RockPaperScissors/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RockPaperScissors
+{
+    class RockPaperScissors
+    {
+        static void Main(string[] args)
+        {
+            //declare variables
+            string inputPlayer, inputCPU;
+            int randomInt;
+
+            Console.WriteLine("Choose between Rock, Paper and Scissors:    ");
+            inputPlayer = Console.ReadLine();
+
+            Random rand = new Random();
+
+            randomInt = rand.Next(1, 4);
+
+            switch (randomInt)
+            {
+                case 1:
+                    inputCPU = "Rock";
+                    Console.WriteLine("Karen chose Rock");
+                    if (inputPlayer == "Rock")
+                    {
+                        Console.WriteLine("Draw!\n\n");
+                    }
+                    else if (inputPlayer == "Paper")
+                    {
+                        Console.WriteLine("Player Wins!\n\n");
+
+                    }
+                    else if (inputPlayer == "Scissors")
+                    {
+                        Console.WriteLine("Karen Wins!\n\n");
+
+                    }
+                    break;
+                case 2:
+                    inputCPU = "Paper";
+                    if (inputPlayer == "Scissors")
+                    {
+                        Console.WriteLine("Karen chose Paper, you win!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Karen chose Paper, try again!");
+                    }
+                    break;
+                case 3:
+                    inputCPU = "Scissors";
+                    if (inputPlayer == "Rock")
+                    {
+                        Console.WriteLine("You Win!");
+                    }
+                    else if (inputPlayer == "Scissors")
+                        Console.WriteLine("Karen chose Scissors, try again!");
+                    break;
+                case 4:
+                    inputCPU = "Rock";
+                    Console.WriteLine("Karen chose Rock, try again!");
+                    break;
+            }
+
+        }
+    }
+}
